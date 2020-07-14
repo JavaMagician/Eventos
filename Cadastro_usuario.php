696a9ad3bb3c1e8c6e1b142630e5715e8a774561
JavaMagician <56736092+JavaMagician@users.noreply.github.com>
2020-07-14 12:36:49 -0300

    Update Cadastro_usuario.php
    Foi feito uma manutenção no código HTML do cadastro de usuarios

--- Cadastro_usuario.php
+++ Cadastro_usuario.php
@@ -12,14 +12,21 @@
     <fieldset>
     <legend>Eventos</legend>
         <label>Nome: </label><br>
-        <input type="text" name="nome" placeholder="Digite seu nome completo"><br>
+        <input type="text" name="nome" placeholder="Digite seu nome completo"
+        pattern="[a-z\s]+$" required="required"><br>
         <label>Telefone: </label><br>
-        <input type="tel" name="telefone" placeholder="(??) ????-????"><br>
+        <input type="tel" name="telefone" placeholder="(??) ????-????" 
+        required="required"
+        maxlength="15" 
+        pattern="\([0-9]{2}\) [0-9]{4,6}-[0-9]{3,4}$"><br>
         <label>Email: </label><br>
-        <input type="email" name="email" placeholder="????@????.???"><br>
+        <input type="email" name="email" placeholder="????@????.???" 
+        required="required" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"><br>
         <label>Senha: </label><br>
-        <input type="password" name="senha" placeholder="Digite uma senha"><br>
-        
+        <input type="password" name="senha" placeholder="Digite uma senha"
+        maxlength="15" minlength="8" required="required" pattern="[A-Za-z0-9]{8,15}"><br>
+        <label>Senha de no minimo 8 caracteres e no máximo 15.
+        </label><br><br>
         <input class="botao" type="submit" value="Cadastrar">
     </form>
 
